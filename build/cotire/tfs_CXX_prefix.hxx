985cd509c4bd289ca050405cf0464702772b76b0
krossom <salinasjoost@gmail.com>
2019-05-24 09:23:43 +0000

    upload files

---
+++ build/cotire/tfs_CXX_prefix.hxx
@@ -0,0 +1,5 @@
+/* cotire.cmake 1.7.6 generated file */
+/* /root/forgottenserver/build/cotire/tfs_CXX_prefix.hxx */
+#ifdef __cplusplus
+#include "/root/forgottenserver/build/cotire/tfs_CXX_prefix.cxx"
+#endif
